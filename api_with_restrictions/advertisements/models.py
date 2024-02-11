0be59b239d29f65e506bb9256e8c54cd3d1129aa
vit555 <litvit555@gmail.com>
2024-02-11 23:53:08 +0700

    First commit

---
+++ api_with_restrictions/advertisements/models.py
@@ -0,0 +1,33 @@
+from django.conf import settings
+from django.contrib.auth.models import User
+from django.db import models
+from django_filters import FilterSet, DateFromToRangeFilter, DateTimeFromToRangeFilter
+
+class AdvertisementStatusChoices(models.TextChoices):
+    """Статусы объявления."""
+
+    OPEN = "OPEN", "Открыто"
+    CLOSED = "CLOSED", "Закрыто"
+    DRAFT = "DRAFT", "Черновик"
+
+
+class Advertisement(models.Model):
+    """Объявление."""
+
+    title = models.TextField()
+    description = models.TextField(default='')
+    status = models.TextField(
+        choices=AdvertisementStatusChoices.choices,
+        default=AdvertisementStatusChoices.OPEN
+    )
+    creator = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )
+    updated_at = models.DateTimeField(
+        auto_now=True
+    )
+    favorite_by = models.ManyToManyField(User, related_name='favorites', blank=True)
---
+++ api_with_restrictions/advertisements/views.py
@@ -0,0 +1,50 @@
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.decorators import action
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
+from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
+
+from advertisements.filters import AdvertisementFilter
+from advertisements.models import Advertisement
+from advertisements.permissions import IsCreatorOrReadOnly
+from advertisements.serializers import AdvertisementSerializer
+
+
+class AdvertisementViewSet(ModelViewSet):
+    """ViewSet для объявлений."""
+
+    # TODO: настройте ViewSet, укажите атрибуты для кверисета,
+    #   сериализаторов и фильтров
+    serializer_class = AdvertisementSerializer
+
+    filter_backends = [DjangoFilterBackend, ]
+    filterset_class = AdvertisementFilter
+    def get_permissions(self):
+        """Получение прав для действий."""
+        if self.action in ["create", "update", "partial_update"]:
+            return [IsAuthenticated()]
+        return []
+
+    def get_queryset(self):
+        us = self.request.user
+        queryset = Advertisement.objects.exclude(status="DRAFT")
+        queryset_draft = Advertisement.objects.filter(status="DRAFT", creator=us.id)
+        if self.request.user.is_authenticated:
+            queryset = queryset | queryset_draft
+        return queryset
+
+    def perform_create(self, serializer):
+        serializer.save(creator=self.request.user)
+
+    @action(methods=['post'], detail=True, url_path='toggle_favorite')
+    def favorite(self, request, *args, **kwargs):
+        post = self.get_object()
+        user = request.user
+        if user.favorites.filter(id=post.id).exists():
+            user.favorites.remove(post)
+        else:
+            if post.creator != user:
+                user.favorites.add(post)
+        return Response({'status': user.favorites.filter(id=post.id).exists()})
+
+
